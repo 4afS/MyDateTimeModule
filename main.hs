043b2d9b99577907ec6a1fa7b74b16eb5beba253
4afs <g2x649@gmail.com>
2018-08-27 15:59:30 +0000

    rename showDT to showDTime

--- main.hs
+++ main.hs
@@ -1,8 +1,8 @@
 import DateTime
 
 main = do
-        putStrLn $ showDT (Time 10 20)
-        putStrLn . showDT $ Time 10 30 <+> Time 5 40
-        putStrLn . showDT $ Time 5 40 <-> Time 24 50
+        putStrLn $ showDTime (Time 10 20)
+        putStrLn . showDTime $ Time 10 30 <+> Time 5 40
+        putStrLn . showDTime $ Time 5 40 <-> Time 24 50
         print $ toTime "2:48"
         print $ toTime "24:60"
